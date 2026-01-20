1cf33a2d6e7df348d08e9783833921a22566b88b
Md Imtiyaz Alam <118051060+imtiyaz1111@users.noreply.github.com>
2026-01-20 20:08:06 +0530

    first commit

--- js/script.js
+++ js/script.js
@@ -93,3 +93,28 @@ if (citySearch) {
     });
   });
 }
+
+// hero-slider
+const slides = document.querySelectorAll('.edu-slide');
+const dots = document.querySelectorAll('.dot');
+let index = 0;
+
+function showSlide(i) {
+  slides.forEach(s => s.classList.remove('active'));
+  dots.forEach(d => d.classList.remove('active'));
+
+  slides[i].classList.add('active');
+  dots[i].classList.add('active');
+}
+
+dots.forEach((dot, i) => {
+  dot.addEventListener('click', () => {
+    index = i;
+    showSlide(index);
+  });
+});
+
+setInterval(() => {
+  index = (index + 1) % slides.length;
+  showSlide(index);
+}, 5000);
